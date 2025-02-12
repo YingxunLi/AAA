7458413707fd85f0dfae4cb3d66b3655409aab88
Yingxun Li <yingxun.li@hfg.design>
2025-02-12 08:21:09 +0100

    1

--- test/1.js
+++ test/1.js
@@ -9,9 +9,11 @@ const baseBallRadius = 100;
 function setup() {
     const canvas = createCanvas(canvasWidth, canvasHeight);
 
+    // 创建 Matter.js 引擎和世界
     engine = Engine.create();
     world = engine.world;
 
+    // 创建摆锤
     pendulum = Bodies.circle(canvasWidth / 2, canvasHeight / 2 + 400, baseBallRadius, {
         restitution: 1,
         density: 3.0,
@@ -19,6 +21,7 @@ function setup() {
         render: { fillStyle: 'transparent' },
     });
 
+    // 创建约束（摆锤的悬挂点）
     let anchor = { x: canvasWidth / 2, y: 0 };
     constraint = Constraint.create({
         pointA: anchor,
@@ -28,8 +31,10 @@ function setup() {
         render: { strokeStyle: 'white', lineWidth: 3 },
     });
 
+    // 将摆锤和约束添加到世界中
     Composite.add(world, [pendulum, constraint]);
 
+    // 创建鼠标约束
     let mouse = Mouse.create(canvas.elt);
     mouseConstraint = MouseConstraint.create(engine, {
         mouse: mouse,
@@ -37,7 +42,17 @@ function setup() {
     });
     Composite.add(world, mouseConstraint);
 
+    // 运行引擎
     Engine.run(engine);
+
+    // 监听鼠标拖拽事件
+    Events.on(mouseConstraint, 'startdrag', function (event) {
+        console.log("开始拖拽！");
+    });
+
+    Events.on(mouseConstraint, 'enddrag', function (event) {
+        console.log("结束拖拽！");
+    });
 }
 
 function draw() {
